c15b5fe86d745482d3658c27bfafcf999f53a9cf
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-06-21 21:49:02 +0530

    Creating Util for Parsing & Validating JWT

--- Hotel-Management-System/backend/src/main/java/com/devstack/backend/util/JwtUtil.java
+++ Hotel-Management-System/backend/src/main/java/com/devstack/backend/util/JwtUtil.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -22,6 +23,15 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, getSigningKey()).compact();
     }
 
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String userName = extractUserName(token);
+        return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
     private Claims extractAllClaims(String token){
         return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
     }
@@ -32,6 +42,10 @@ public class JwtUtil {
 
     private Date extractExpiration(String token){
         return extractClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers){
