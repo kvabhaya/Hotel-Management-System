bf5c35452dbaee701fb7acfcb40552245cb9d636
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-06-27 22:56:43 +0530

    nz-sider updated

--- Hotel-Management-System/frontend/src/app/app.module.ts
+++ Hotel-Management-System/frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NzLayoutModule} from "ng-zorro-antd/layout";
 
 registerLocaleData(en);
 
@@ -17,13 +18,14 @@ registerLocaleData(en);
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NzLayoutModule
+    ],
   providers: [
     { provide: NZ_I18N, useValue: en_US }
   ],
